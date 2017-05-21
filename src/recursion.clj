f0f946cc089197e479642d56769c201ea2f84c51
jaycee14 <josh@jjcasswell.com>
2017-05-21 09:35:49 +0100

    update

--- src/recursion.clj
+++ src/recursion.clj
@@ -72,29 +72,47 @@
 (defn seq= [a-seq b-seq]
   (cond
    (and (empty? a-seq) (empty? b-seq)) true
+   (or (empty? a-seq) (empty? b-seq)) false
    (= (first a-seq) (first b-seq)) (seq= (rest a-seq) (rest b-seq))
    :else false))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (if (or (empty? seq-1) (empty? seq-2))
+    ()
+    (cons 
+     (f (first seq-1) (first seq-2)) 
+     (my-map f (rest seq-1) (rest seq-2)))
+    ))
 
 (defn power [n k]
-  :-)
+  (if (zero? k)
+    1
+    (* n (power n (dec k)))))
 
 (defn fib [n]
-  :-)
+  (if (< n 2)
+    n
+    (+ (fib (- n 1)) (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
-  [:-])
+  (if (< how-many-times 1)
+    ()
+    (cons what-to-repeat (my-repeat (dec how-many-times) what-to-repeat))))
 
 (defn my-range [up-to]
-  [:-])
+  (if (< up-to 1)
+    ()
+    (let [n (dec up-to)]
+      (cons n (my-range n)))))
 
 (defn tails [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    (list ())
+    (cons (seq a-seq) (tails (rest a-seq)))))
 
 (defn inits [a-seq]
-  [:-])
+  (let [r (reverse a-seq)]
+    (map reverse (tails r))))
 
 (defn rotations [a-seq]
   [:-])
